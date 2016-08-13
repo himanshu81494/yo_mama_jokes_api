e41332f683523dd4aaa0a29b27b12eedaa9e102f
himanshu81494 <himanshu81494@gmail.com>
2016-08-13 23:58:48 +0530

    initial

---
+++ joke.py
@@ -0,0 +1,37 @@
+import random
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+from sqlalchemy import create_engine
+e = create_engine('sqlite:///jokes.db')
+app = Flask(__name__)
+api = Api(app)
+
+def insert():
+  fname = 'jokes.txt'
+  with open(fname) as f:
+    content = f.readlines()
+  # print random.choice(content)[:-1]
+  conn = e.connect()
+  for item in content:
+    data = "insert into jokes(joke) values(" + (dumps(item[:-1])) + ")"
+    query = conn.execute(data)
+    # print data
+  print query
+
+class getRandomJoke(Resource):
+  def get(self):
+    conn = e.connect()
+    query = conn.execute("select joke from jokes order by RANDOM() LIMIT 1;")
+    return {'joke': [i[0] for i in query.cursor.fetchall()]}
+class Home(Resource):
+  def get(self):
+    return "Welcome"
+
+api.add_resource(getRandomJoke, '/joke')
+api.add_resource(Home, '/')
+
+if __name__ == '__main__':
+  app.run()
+
+# insert()
